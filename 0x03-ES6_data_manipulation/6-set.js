18079d855010a6b6ae94e6ef029b2cc215b874f5
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-12-05 15:36:20 -0500

    all tasks

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Kwenziwa Khanyile <https://github.com/kwenziwa>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
