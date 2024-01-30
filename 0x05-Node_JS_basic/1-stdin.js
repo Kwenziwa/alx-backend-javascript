600e69675d6c407a63c8e1b0e4c4899a792cd119
Kwenziwa Lizwi <Kwenziwa@live.com>
2024-01-30 15:30:45 -0500

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-const readline = require("readline");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const input_name = process.stdin.read();
+
+  if (input_name) {
+    process.stdout.write(`Your name is: ${input_name}`);
+  }
 });
 
-console.log("Welcome to Holberton School, what is your name?\n");
-
-rl.on("line", (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-});
-
-rl.on("close", () => {
-  console.log("This important software is now closing\n");
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
